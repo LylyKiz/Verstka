137e5aac74827c29884791a8e38548ca99f5ffe9
LylyKiz <Sarkli@mail.ru>
2019-05-20 01:13:42 +0500

    second commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    $("form").submit(function () {
+        // Получение ID формы
+        var formID = $(this).attr('id');
+        // Добавление решётки к имени ID
+        var formNm = $('#' + formID);
+        $.ajax({
+            type: "POST",
+            url: 'mail.php',
+            data: formNm.serialize(),
+            success: function (data) {
+                // Вывод текста результата отправки
+                $(formNm).html(data); 
+            },
+            error: function (jqXHR, text, error) {
+                // Вывод текста ошибки отправки
+                $(formNm).html(error);         
+            }
+        });
+        return false;
+    });
+	$(".regular").slick({
+		dots: true,
+		infinite: true,
+		slidesToShow: 2,
+		slidesToScroll: 2
+	});
+
+});
